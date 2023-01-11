f3fc45a2c19ecdf96c1c83997dfb712c88cb1657
Amy Lourdes Ortega De León <79715201+Amy1909@users.noreply.github.com>
2023-01-11 12:02:53 -0600

    Add files via upload

---
+++ js/ventana.js
@@ -0,0 +1,66 @@
+//VINCULAR MI MENÚ
+var open = document.getElementById ('open');
+var cafeinfo = document.getElementById ('cafeinfo');
+
+//FUNCIÓN
+function activar () {
+    cafeinfo.classList.add ("activacafeinfo");
+}
+//SUCESO
+open.addEventListener ('click', activar);
+
+
+
+
+//VINCULAR MI MENÚ
+var open2 = document.getElementById ('open2');
+var cafeincatalgofo = document.getElementById ('catalgo');
+
+//FUNCIÓN
+function activar2 () {
+    catalgo.classList.add ("activacatalgoinfo");
+}
+//SUCESO
+open2.addEventListener ('click', activar2);
+
+
+
+
+//VINCULAR MI MENÚ
+var open3 = document.getElementById ('open3');
+var photo = document.getElementById ('photo');
+
+//FUNCIÓN
+function activar3 () {
+    photo.classList.add ("activaphoto");
+}
+//SUCESO
+open3.addEventListener ('click', activar3);
+
+
+
+
+//VINCULAR MI MENÚ
+var open4 = document.getElementById ('open4');
+var ilustraitor = document.getElementById ('ilustraitor');
+
+//FUNCIÓN
+function activar4 () {
+    ilustraitor.classList.add ("activailustritor");
+}
+//SUCESO
+open4.addEventListener ('click', activar4);
+
+
+
+
+//VINCULAR MI MENÚ
+var open6 = document.getElementById ('open6');
+var infoweb = document.getElementById ('infoweb');
+
+//FUNCIÓN
+function activar6 () {
+    infoweb.classList.add ("activainfoweb");
+}
+//SUCESO
+open6.addEventListener ('click', activar6);
